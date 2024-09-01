ace3c2e4e351bc82ba824af8a5b4daa516a12834
qwert <qwert@gmail.com>
2024-09-01 22:12:39 +0500

    version1.4

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -25,7 +25,6 @@ const Home: FC = () => {
     { id: 4, text: 'Количество по убыванию', value: 'stock', order: 'desc' },
     { id: 5, text: 'Сбросить', value: '', order: '' },
   ]
-  
   const handlePageClick = (event: { selected: number }) => {
     const section = document.getElementById('home');
     section!.scrollIntoView({ behavior: 'instant' })
